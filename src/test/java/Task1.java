305325f11bcd265bc85b10e470c5285927902725
sawgreenkiller <giorgi.bazierashvili@gmail.com>
2022-06-04 12:56:53 +0400

    first commit - added WebDriverManager and singleton initialization mode for webdriver

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,7 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -14,34 +17,27 @@ public class Task1  {
 
     @BeforeTest
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://store.steampowered.com/");
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
+
+        TestBase.initialize();
 
     }
     @Test(priority = 1)
     public void mainPageCheckTest(){
-        WebElement FullscreenBG = driver.findElement(By.xpath("//div[@class = 'fullscreen-bg']"));
-        Assert.assertTrue(FullscreenBG.isDisplayed());
-    }
 
-    @Test(priority = 2)
-    public void aboutPageCheckTest() throws InterruptedException {
-        WebElement aboutButton = driver.findElement(By.xpath("//a[contains(text(),'ABOUT')]"));
+        //step1
+        WebElement FullscreenBG = TestBase.driver.findElement(By.xpath("//div[@class = 'fullscreen-bg']"));
+        Assert.assertTrue(FullscreenBG.isDisplayed());
+
+        //step2
+        WebElement aboutButton = TestBase.driver.findElement(By.xpath("//a[contains(text(),'ABOUT')]"));
         aboutButton.click();
-        WebElement aboutSteam = driver.findElement(By.xpath("//div[contains(@class,\"about_subtitle\")]"));
+        WebElement aboutSteam = TestBase.driver.findElement(By.xpath("//div[contains(@class,\"about_subtitle\")]"));
 
         Assert.assertTrue(aboutSteam.isDisplayed());
 
-
-    }
-
-    @Test(priority = 3)
-    public void compareOnlineAndActive(){
-        WebElement onlineStatElement = driver.findElement(By.xpath("//div[@class='online_stat'][1]"));
-        WebElement inGameStatElement = driver.findElement(By.xpath("//div[contains(@class,'gamers_in_game')]//parent::div[@class ='online_stat']"));
+        //step3
+        WebElement onlineStatElement = TestBase.driver.findElement(By.xpath("//div[@class='online_stat'][1]"));
+        WebElement inGameStatElement = TestBase.driver.findElement(By.xpath("//div[contains(@class,'gamers_in_game')]//parent::div[@class ='online_stat']"));
 
         //this converts webelements to string
         String onlineStatString = onlineStatElement.getText();
@@ -60,21 +56,20 @@ public class Task1  {
 
         Assert.assertTrue(inGameInt<onlineInt);
 
-    }
-
-    @Test(priority=4)
-    public void StorePageCheckTest(){
-        WebElement StoreButtonElement = driver.findElement(By.xpath("//a[contains(text(),'STORE')]"));
+        //step4
+        WebElement StoreButtonElement = TestBase.driver.findElement(By.xpath("//a[contains(text(),'STORE')]"));
         StoreButtonElement.click();
         //this was copy pasted from first test case because basically they both check that unique background poster is displayed :)
-        WebElement FullscreenBG = driver.findElement(By.xpath("//div[@class = 'fullscreen-bg']"));
+         FullscreenBG = TestBase.driver.findElement(By.xpath("//div[@class = 'fullscreen-bg']"));
         Assert.assertTrue(FullscreenBG.isDisplayed());
 
 
+
     }
+
     @AfterTest
     public void tearDown(){
-        driver.quit();
+        TestBase.quit();
     }
 
 }
